f3c4f644de810a1372ee34bc18fc15fcc6b400f3
Ajay <ajaykaarangupta11@gmail.com>
2022-09-10 16:22:36 +0530

    login code corrected

--- src/main/java/com/interviewdeck/services/ValidateUser.java
+++ src/main/java/com/interviewdeck/services/ValidateUser.java
@@ -6,7 +6,7 @@ public class ValidateUser {
     public static boolean isValidUser(LoginDTO loginDTO) {
         // fetch from database for the userName (username and password
         System.out.println(loginDTO.toString());
-        String userName="", password="";
-        return "user1".equals(userName) && "pwd".equals(password);
+//        String userName="", password="";
+        return "user1".equals(loginDTO.getUserName()) && "pwd".equals(loginDTO.getPassword());
     }
 }
