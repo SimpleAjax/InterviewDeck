f04a71b4f116dfded5af0a10c92f9c20a3493f79
venkateshatchutha <venkateswararao.atchutha@gmail.com>
2022-09-11 00:43:25 +0530

    Added update deck code

--- src/main/java/com/interviewdeck/repository/DeckRepository.java
+++ src/main/java/com/interviewdeck/repository/DeckRepository.java
@@ -3,10 +3,13 @@ package com.interviewdeck.repository;
 import com.interviewdeck.models.Company;
 import com.interviewdeck.models.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface DeckRepository extends JpaRepository<Deck, Integer>, CustomDeckRepository {
+public interface DeckRepository extends JpaRepository<Deck, Long> {
+
     List<Deck> getByCompany(Company company);
+
 }
